9fa752f6b446c8f50881543c016777dd82acab1a
miznat <apps.miznat@gmail.com>
2014-08-10 03:58:28 +1000

    add plus sign between words

--- geocoder.py
+++ geocoder.py
@@ -32,3 +32,56 @@ def getDataFromCSV(names,addresses,phoneNumbers,cuisines,eatingOptions):
 	     	phoneNumbers.append(row[2])
 	     	cuisines.append(row[3])
 	     	eatingOptions.append(row[4])
+
+
+def addPlusSignBetweenWords(name,address):
+
+		# Create an array for name with each word as an item
+		name = re.compile('\w+').findall(name)
+
+		nameWithPlusBetweenWords = []
+
+		# Create a new list and add '+' between each word (each item in the list)
+
+		for word in name:
+			
+			nameWithPlusBetweenWords.append(word)
+			nameWithPlusBetweenWords.append("+")
+
+		# Create an array for address with each word as an item
+
+		address = re.compile('\w+').findall(address)
+
+		addressWithPlusBetweenWords = []
+
+		# Create a new list and add '+' between each word (each item in the list)
+
+		for word in address:
+
+			addressWithPlusBetweenWords.append(word) 
+			
+			addressWithPlusBetweenWords.append("+")
+			
+		# Remove the last item in the list, which is '+'
+
+		addressWithPlusBetweenWords.pop() 
+
+		# Take the two lists and join them
+
+		b = nameWithPlusBetweenWords + addressWithPlusBetweenWords
+
+		# Take the new list 'b' and create a string
+
+		b = ''.join(b)
+
+		return b
+
+getDataFromCSV(names,addresses,phoneNumbers,cuisines,eatingOptions)
+
+
+
+
+
+
+
+
